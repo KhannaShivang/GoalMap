975848e61e9d4b47a1eef90e48efa709bab5efff
KhannaShivang <kshivang343@gmail.com>
2026-04-07 18:20:46 +0530

    Fixed

--- src/test/java/com/aigoalplanner/AiGoalPlannerApplicationTests.java
+++ src/test/java/com/aigoalplanner/AiGoalPlannerApplicationTests.java
@@ -2,7 +2,7 @@ package com.aigoalplanner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 // @ActiveProfiles("test")
